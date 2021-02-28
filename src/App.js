a9d519c922d91799f156e3b64f29e3156eda5740
Guillim <guigloo@msn.com>
2021-02-28 16:45:38 +0100

    The routes are now created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+import Calendar from './Calendar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <header className="App-header">
+            <p>
+              Follow the link bellow to select your meeting. You will be able to choose a time by click on a timeslot.
+            </p>
+            <Link to='/calendar'>
+              <span
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer">here
+              </span>
+            </Link>
+          </header>
+        </Route>
+        <Route exact path="/calendar">
+          <Calendar/>
+        </Route>
+        <Route path='*'>
+          <p>not found</p>
+        </Route>
+      </Switch>
+      </Router>
     </div>
   );
 }
